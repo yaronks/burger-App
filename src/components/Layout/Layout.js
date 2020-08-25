cba48a2805ae5bd08c5614f2b1baf0b3a030b459
Yaron Sternberg <yaronsternberg@Yarons-Air.fios-router.home>
2020-08-25 14:52:27 -0400

    Added SideDrawer and Navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import '../Layout/Layout.css';
+import Toolbar from './../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = ( props ) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <div>
+            <Toolbar/>
+            <SideDrawer/>
+        </div>
         <main className="Content">
             {props.children}
         </main> 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Toolbar.css'
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
-    <header>
+    <header className="Toolbar">
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo height="80%"/>
         <nav>
-            ...
+            <NavigationItems/>
         </nav>
     </header>
 );
